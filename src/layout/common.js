c5f69bfe6b3e9bcf9781563e175a83540326b214
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-02-07 22:19:23 +0800

    架构魔改 5 学习观察架构 Utils

--- src/layout/common.js
+++ src/layout/common.js
@@ -1,24 +1,33 @@
 import {defineStore} from 'pinia'
 import router from '@/router/index.js'
 
+// 文件分类
+export const fileTypeMap = {
+  image: ['gif', 'jpg', 'jpeg', 'png', 'bmp', 'webp', 'ico'],
+  video: ['mp4', 'webm', 'm3u8', 'rmvb', 'avi', 'swf', '3gp', 'mkv', 'flv'],
+  audio: ['mp3', 'wav', 'wma', 'ogg', 'aac', 'flac', 'm4a'],
+  text: ['scss', 'sass', 'kt', 'gitignore', 'bat', 'properties', 'yml', 'css', 'js', 'md', 'xml', 'txt', 'py', 'go', 'html', 'less', 'php', 'rb', 'rust', 'script', 'java', 'sh', 'sql'],
+  executable: ['exe', 'dll', 'com', 'vbs'],
+  archive: ['7z', 'zip', 'rar', 'tar', 'gz'],
+  pdf: ['pdf'],
+  office: ['doc', 'docx', 'csv', 'xls', 'xlsx', "ppt", 'pptx'],
+  three3d: ['dae', 'fbx', 'gltf', 'glb', 'obj', 'ply', 'stl'],
+  document: ['txt', 'pages', 'epub', 'numbers', 'keynote']
+};
+
+
 export const useCommonStore = defineStore('common', {
   state: () => {
     return {
       // 页面文档可视高度(随窗口改变大小)
-      documentClientHeight: 0,
-      // 侧边栏, 布局皮肤, light(浅色) / dark(黑色)
-      sidebarLayoutSkin: 'light',
-      // 侧边栏, 折叠状态
-      sidebarFold: true,
-      // 侧边栏, 菜单
+      documentClientHeight: 0, // 侧边栏, 布局皮肤, light(浅色) / dark(黑色)
+      sidebarLayoutSkin: 'light', // 侧边栏, 折叠状态
+      sidebarFold: true, // 侧边栏, 菜单
       menuList: [],
-      menuActiveName: '',
-      // 主入口标签页
+      menuActiveName: '', // 主入口标签页
       mainTabs: [],
-      mainTabsActiveName: '',
-      // 当前选择的标签
-      selectMenu: [],
-      // 路由列表
+      mainTabsActiveName: '', // 当前选择的标签
+      selectMenu: [], // 路由列表
       routeList: [],
       menuIds: [],
       selectLeftId: '',
