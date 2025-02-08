d476211e729dcb1aff9d2e13ccd46ac83d53876f
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-02-08 20:09:42 +0800

    打扫屋子 10 Trash

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,5 @@
   </el-config-provider>
 </template>
 
-<style lang="scss">
-.card-prod-info-btn {
-  font-size: 14px;
-
-  .prod-name {
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    padding: 0 8px;
-  }
-
-  .del-btn {
-    line-height: 20px;
-    text-align: right;
-    padding-right: 8px;
-    user-select: none;
-
-    span {
-      color: #155bd4;
-      cursor: pointer;
-
-      &:hover {
-        opacity: 0.8;
-      }
-
-      &.disabled {
-        opacity: 0.6;
-        cursor: not-allowed;
-      }
-    }
-  }
-}
-
+<style lang="scss" scoped>
 </style>
